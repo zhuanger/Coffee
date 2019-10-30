eaf32de93a9111540dc1995836be22be1cc3113f
linweicong <984145377@qq.com>
2019-10-30 19:39:57 +0800

    添加照片/更改样式

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -2,14 +2,21 @@
   <div class="about">
     <img src="@A/images/aboutMe.png" alt="" class="about-img">
     <div class="about-contanier" :style="'width:' + clientWidth">
-      <div>
-        <div class="about-container-left">
+      <div class="about-container-div">
+        <div class="about-container-div-left">
           <ul>
             <li v-for="(item, index) of liItem" :key="index" :style="'background-color:' + (item.isActive ? '#eee' : '#fff')"
               @click="changeTab(index)">
-              <a :class="item.isActive ? item.activeUrl : url ">{{item.name}}</a>
+              <a :class="item.isActive ? item.activeUrl : item.url ">{{item.name}}</a>
             </li>
           </ul>
+        </div>
+        <div class="about-container-div-right">
+          <div>
+            <h2>商业模式</h2>
+            <p></p>
+            <img src="" alt="">
+          </div>
         </div>
       </div>
     </div>
@@ -21,8 +28,8 @@
       return{
         clientWidth: window.innerWidth + 'px',
         clickActive: 0,
-        liItem: [{name: '关于我们', isActive: true,url: 'aboutIcon', activeUrl: 'aboutIconActive'}, {name: '联系我们', isActive: false},
-                  {name: '加入我们', isActive: false}
+        liItem: [{name: '关于我们', isActive: true,url: 'aboutIcon', activeUrl: 'aboutIconActive'}, {name: '联系我们', isActive: false, url:'contactIcon', activeUrl:'contactIconActive'},
+                  {name: '加入我们', isActive: false, url: 'joinIcon', activeUrl: 'joinIconActive'}
                 ],
       }
     },
@@ -49,40 +56,59 @@
       max-height: 340px;
     }
     &-contanier{
-      padding: 100px 140px 0 140px;
-      >div{
+      padding-top: 100px;
+      &-div{
         border: 1px solid red;
-        width: 100%;
+        width: 900px;
         margin: 0 auto;
-        ul{
-          width: 20%;
-          li{
-            padding: 15px 25px;
-            text-align: center;
-            cursor: pointer;
-            // background-color: #eee;
-            a{
-              position: relative;
-            }
-            a::before{
-              content: '';
-              position: absolute;
-              width: 30px;
-              height: 30px;
-              left: -40px;
-              top: -6px;
-              background-size: 30px 30px; 
-            }
-            .aboutIconActive::before{
-              background-image: url('../../assets/images/icon01_active.png');         
-            }
-            .aboutIcon::before{
-              background-image: url('../../assets/images/icon01.png');         
-            }
-          }
-        }
+        display: flex;
+        // &-left{
+        //   ul{
+        //     width: 100%;
+        //     li{
+        //       padding: 10px 60px;
+        //       text-align: center;
+        //       cursor: pointer;
+        //       a{
+        //         position: relative;
+        //       }
+        //       a::before{
+        //         content: '';
+        //         position: absolute;
+        //         width: 23px;
+        //         height: 23px;
+        //         left: -40px;
+        //         top: -1px;
+        //         background-size: 23px 23px; 
+        //       }
+        //       .aboutIconActive::before{
+        //         background-image: url('../../assets/images/icon01_active.png');         
+        //       }
+        //       .aboutIcon::before{
+        //         background-image: url('../../assets/images/icon01.png');         
+        //       }
+        //       .contactIconActive::before{
+        //         background-image: url('../../assets/images/contact01_active.png');         
+        //       }
+        //       .contactIcon::before{
+        //         background-image: url('../../assets/images/contact01.png');         
+        //       }
+        //       .joinIconActive::before{
+        //         background-image: url('../../assets/images/joinus_active.png');         
+        //       }
+        //       .joinIcon::before{
+        //         background-image: url('../../assets/images/joinus.png');         
+        //       }
+        //     }
+        //   }
+        // }
       }
       
+      // &-div{
+      //   &-right{
+      //     padding: 50px;
+      //   }
+      // }
     }
   }
 </style>
