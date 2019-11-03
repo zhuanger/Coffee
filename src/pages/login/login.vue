331dc92932aba97c6046c6b4b9e9e31620310770
unknown <984145377@qq.com>
2019-11-03 18:37:32 +0800

    登陆

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -2,18 +2,65 @@
   <div class="login">
     <div class="login-top">
       <div class="login-top-box">
-        <img src="" alt="">
+        <img src="@A/images/logo.png" alt="" class="login-top-box-img">
         <h3 class="login-top-box-h3">登陆</h3>
       </div>
     </div>
     <div class="login-container">
-
+      <h3>系统登陆</h3>
+      <div class="login-container-box">
+        <el-input placeholder="您的员工账号" v-model="username" clearable class="login-container-input" maxlength="11"></el-input>
+        <span for="">{{errUsernameText}}</span>
+      </div>
+      <div class="login-container-box">
+        <el-input placeholder="您的密码" v-model="password" clearable show-password maxlength="13" class="login-container-input"></el-input>          
+        <span for="">{{errPasswordText}}</span>
+      </div>
+      <a href="#" class="login-container-a">忘记密码</a>
+      <el-button type="primary" class="login-container-button" @click="login">登录</el-button>
     </div>
   </div>
 </template>
 <script>
   export default {
-    
+    name: 'login',
+    data(){
+      return{
+        username: '',
+        password: '',
+        errUsernameText: '',
+        errPasswordText: '',
+      }
+    },
+    methods: {
+      login(){
+        let username = this.username.replace(/\s/g, ''), password = this.password.replace(/\s/g, ''), self = this;
+        if(username.length === 0){
+          this.errUsernameText = '请输入账号';
+          setTimeout(() => {
+            self.hideErrText('username')
+          }, 1500);
+          return;
+        }else if(password.length === 0){
+          this.errPasswordText = '请输入密码';
+          setTimeout(() => {
+            self.hideErrText('password')
+          }, 1500);
+          return;
+        }
+      },
+      hideErrText(type){
+        let errType = {
+          'username': ()=>{
+            this.errUsernameText = ''
+          },
+          'password': ()=>{
+            this.errPasswordText = ''           
+          },
+        };
+        errType[type]();
+      }
+    },
   }
 </script>
 <style lang="scss" scoped>
@@ -21,15 +68,60 @@
     &-top{
       width: 100%;
       height: 270px;
-      background-color: #eee;
+      background-color: $bg-color;
       &-box{
+        position: relative;
+        top: 230px;
         @include mar-center(200px);
+        border-top: 1px solid #f8f8f8;
+        &-img{
+          position: absolute;
+          top: -130px;
+          right: 50px;
+          width: 100px;
+          height: 100px;
+        }
         &-h3{
-          border-top: 1px solid #f8f8f8;
           text-align: center;
+          position: relative;
+          top: -14px;
+          @include mar-center(75px);
+          background-color: $bg-color;
         }
       }
       
+    }
+    &-container{
+      @include mar-center(280px);
+      margin-top: 100px;
+      >h3{
+        margin-bottom: 20px;
+      }
+      &-box{
+        padding-bottom: 35px;
+        position: relative;
+        >span{
+          color: red;
+          font-size: 14px;
+          position: absolute;
+          bottom: 15px;
+          left: 0px;
+        }
+      }
+      &-input{
+        // margin-bottom: 15px;
+      }
+      &-a{
+        color: $a-color;
+        position: relative;
+        left: 213px;
+        top: -26px;
+        font-size: 14px;
+      }
+      &-button{
+        display: block;
+        margin: 0 auto;
+      }
     }
   }
 </style>
